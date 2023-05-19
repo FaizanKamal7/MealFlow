d474a7c9ab9a03912a5e9bdd71912468e2b74575
FaizanKamal7 <57044214+FaizanKamal7@users.noreply.github.com>
2023-05-19 12:18:47 +0500

    Routes connectivity and UI tweaks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,9 @@
-<!DOCTYPE html>
+<html>
+    {{-- @include('oomph') --}}
+    @include('nexgen_home_view')
+</html>
+
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -129,4 +134,4 @@
             </div>
         </div>
     </body>
-</html>
+</html> --}}
