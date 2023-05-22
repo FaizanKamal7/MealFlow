5b1fd7fec1a635b03f52170f731633d6a5c79259
zee404 <62300073+zee404@users.noreply.github.com>
2023-05-22 12:59:09 +0500

    Fleet Service Initialization

---
+++ Modules/FleetService/Database/Migrations/2023_05_19_094012_create_vehicle_maintenances_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicle_maintenances', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('vehicle_id');
+            $table->date('maintenance_date');
+            $table->text('description');
+            $table->decimal('cost', 10, 2);
+
+            $table->foreign('vehicle_id')->references('id')->on('vehicles')->onDelete("set null");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicle_maintenances');
+    }
+};
