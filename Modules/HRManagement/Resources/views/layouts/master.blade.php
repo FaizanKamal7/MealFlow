dc62547519e40d473547f828e126139bd2691360
Muhammad Usman <usmanamin000@gmail.com>
2023-05-22 15:05:53 +0500

    HR settings

--- Modules/HRManagement/Resources/views/layouts/master.blade.php
+++ Modules/HRManagement/Resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 <!--begin::Head-->
 <head>
     <base href="">
-    <title>@yield('title') - Nixus ERP</title>
+    <title>@yield('title') - Real Estate ERP</title>
     <meta charset="utf-8"/>
     <meta name="description" content=""/>
     <meta name="keywords" content=""/>
@@ -14,8 +14,10 @@
     <meta property="og:url" content=""/>
     <meta property="og:site_name" content=""/>
     <link rel="canonical" href=""/>
-    <link rel="shortcut icon" href="{{ asset('static/media/logx-logo-white.png') }}"/>
+    <link rel="shortcut icon" href=""/>
     @include('partials.styles')
+    @yield("extra_style")
+
 </head>
 <!--end::Head-->
 <!--begin::Body-->
@@ -36,7 +38,7 @@
             <div class="aside-logo flex-column-auto" id="kt_aside_logo">
                 <!--begin::Logo-->
                 <a href="#">
-                    <img alt="Logo" src="{{ asset('static/media/logx-logo-white.png') }}" class="h-35px logo"/>
+                    <img alt="Logo" src="{{ asset('static/media/arazeelogo.png') }}" class="h-35px logo"/>
                 </a>
                 <!--end::Logo-->
                 <!--begin::Aside toggler-->
@@ -843,33 +845,29 @@
 
                 </div>
                 <!--end::Menu-->
-        </div>
-        <!--end::Aside Menu-->
-    </div>
-    <!--end::Aside menu-->
-    @show
+            @show
             @section('sidebar_footer')
                 <!--begin::Footer-->
                 <div class="aside-footer flex-column-auto pt-5 pb-7 px-5" id="kt_aside_footer">
-{{--                    <a href="mailto:help@nixus.com" class="btn btn-custom btn-warning w-100"--}}
-{{--                       data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss-="click"--}}
-{{--                       title="contact us in case of any issues" >--}}
-{{--                        <span class="btn-label">Contact Support</span>--}}
-{{--                        <!--begin::Svg Icon | path: icons/duotune/general/gen005.svg') }}-->--}}
-{{--                        <span class="svg-icon btn-icon svg-icon-2">--}}
-{{--								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"--}}
-{{--                                     fill="none">--}}
-{{--									<path opacity="0.3"--}}
-{{--                                          d="M19 22H5C4.4 22 4 21.6 4 21V3C4 2.4 4.4 2 5 2H14L20 8V21C20 21.6 19.6 22 19 22ZM12.5 18C12.5 17.4 12.6 17.5 12 17.5H8.5C7.9 17.5 8 17.4 8 18C8 18.6 7.9 18.5 8.5 18.5L12 18C12.6 18 12.5 18.6 12.5 18ZM16.5 13C16.5 12.4 16.6 12.5 16 12.5H8.5C7.9 12.5 8 12.4 8 13C8 13.6 7.9 13.5 8.5 13.5H15.5C16.1 13.5 16.5 13.6 16.5 13ZM12.5 8C12.5 7.4 12.6 7.5 12 7.5H8C7.4 7.5 7.5 7.4 7.5 8C7.5 8.6 7.4 8.5 8 8.5H12C12.6 8.5 12.5 8.6 12.5 8Z"--}}
-{{--                                          fill="currentColor"/>--}}
-{{--									<rect x="7" y="17" width="6" height="2" rx="1" fill="currentColor"/>--}}
-{{--									<rect x="7" y="12" width="10" height="2" rx="1" fill="currentColor"/>--}}
-{{--									<rect x="7" y="7" width="6" height="2" rx="1" fill="currentColor"/>--}}
-{{--									<path d="M15 8H20L14 2V7C14 7.6 14.4 8 15 8Z" fill="currentColor"/>--}}
-{{--								</svg>--}}
-{{--							</span>--}}
-{{--                        <!--end::Svg Icon-->--}}
-{{--                    </a>--}}
+                    <a href="/" class="btn btn-custom btn-warning w-100"
+                       data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss-="click"
+                       title="visit Araazee.com" style="background-color: #EF7F22;">
+                        <span class="btn-label">Visit Website</span>
+                        <!--begin::Svg Icon | path: icons/duotune/general/gen005.svg') }}-->
+                        <span class="svg-icon btn-icon svg-icon-2">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                     fill="none">
+									<path opacity="0.3"
+                                          d="M19 22H5C4.4 22 4 21.6 4 21V3C4 2.4 4.4 2 5 2H14L20 8V21C20 21.6 19.6 22 19 22ZM12.5 18C12.5 17.4 12.6 17.5 12 17.5H8.5C7.9 17.5 8 17.4 8 18C8 18.6 7.9 18.5 8.5 18.5L12 18C12.6 18 12.5 18.6 12.5 18ZM16.5 13C16.5 12.4 16.6 12.5 16 12.5H8.5C7.9 12.5 8 12.4 8 13C8 13.6 7.9 13.5 8.5 13.5H15.5C16.1 13.5 16.5 13.6 16.5 13ZM12.5 8C12.5 7.4 12.6 7.5 12 7.5H8C7.4 7.5 7.5 7.4 7.5 8C7.5 8.6 7.4 8.5 8 8.5H12C12.6 8.5 12.5 8.6 12.5 8Z"
+                                          fill="currentColor"/>
+									<rect x="7" y="17" width="6" height="2" rx="1" fill="currentColor"/>
+									<rect x="7" y="12" width="10" height="2" rx="1" fill="currentColor"/>
+									<rect x="7" y="7" width="6" height="2" rx="1" fill="currentColor"/>
+									<path d="M15 8H20L14 2V7C14 7.6 14.4 8 15 8Z" fill="currentColor"/>
+								</svg>
+							</span>
+                        <!--end::Svg Icon-->
+                    </a>
                 </div>
                 <!--end::Footer-->
             @show
@@ -961,7 +959,7 @@
                                     <!--end::Menu item-->
                                     <!--begin::Menu item-->
                                     <div class="menu-item px-5">
-                                        <a href="#"
+                                        <a href="{{ route("logout") }}"
                                            class="menu-link px-5">Sign Out</a>
                                     </div>
                                     <!--end::Menu item-->
@@ -985,31 +983,31 @@
 
             </div>
             <!--end::Content-->
-{{--            @section('footer')--}}
-{{--                <!--begin::Footer-->--}}
-{{--                <div class="footer py-4 d-flex flex-lg-column" id="kt_footer">--}}
-{{--                    <!--begin::Container-->--}}
-{{--                    <div--}}
-{{--                        class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">--}}
-{{--                        <!--begin::Copyright-->--}}
-{{--                        <div class="text-dark order-2 order-md-1">--}}
-{{--                            <span class="text-muted fw-bold me-1">2023 ©</span>--}}
-{{--                            <a href="#" target="_blank" class="text-gray-800 text-hover-primary">Nixus ERP</a>--}}
-{{--                        </div>--}}
-{{--                        <!--end::Copyright-->--}}
-{{--                        <!--begin::Menu-->--}}
-{{--                        <ul class="menu menu-gray-600 menu-hover-primary fw-bold order-1">--}}
-{{--                            <li class="menu-item">--}}
-{{--                                <a href="#" target="_blank" class="menu-link px-2">Support</a>--}}
-{{--                            </li>--}}
+            @section('footer')
+                <!--begin::Footer-->
+                <div class="footer py-4 d-flex flex-lg-column" id="kt_footer">
+                    <!--begin::Container-->
+                    <div
+                        class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
+                        <!--begin::Copyright-->
+                        <div class="text-dark order-2 order-md-1">
+                            <span class="text-muted fw-bold me-1">2023 ©</span>
+                            <a href="#" target="_blank" class="text-gray-800 text-hover-primary">Real Estate ERP</a>
+                        </div>
+                        <!--end::Copyright-->
+                        <!--begin::Menu-->
+                        <ul class="menu menu-gray-600 menu-hover-primary fw-bold order-1">
+                            <li class="menu-item">
+                                <a href="#" target="_blank" class="menu-link px-2">Support</a>
+                            </li>
 
-{{--                        </ul>--}}
-{{--                        <!--end::Menu-->--}}
-{{--                    </div>--}}
-{{--                    <!--end::Container-->--}}
-{{--                </div>--}}
-{{--                <!--end::Footer-->--}}
-{{--            @show--}}
+                        </ul>
+                        <!--end::Menu-->
+                    </div>
+                    <!--end::Container-->
+                </div>
+                <!--end::Footer-->
+            @show
         </div>
         <!--end::Wrapper-->
     </div>
