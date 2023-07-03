46b97a08118d1ef5ad1f15c2b9fdaba67e892d2f
Muhammad Usman <usmanamin000@gmail.com>
2023-07-03 23:28:08 +0500

    Delivery Service

--- config/app.php
+++ config/app.php
@@ -186,6 +186,7 @@ return [
          * Package Service Providers...
          */
 
+        SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -211,6 +212,7 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
         'Gate' => Illuminate\Support\Facades\Gate::class,
+        'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class
     ])->toArray(),
 
 ];
