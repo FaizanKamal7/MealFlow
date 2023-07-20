82a9cf5daffd62ca58d520e7763d3c912021f0ea
zee404 <62300073+zee404@users.noreply.github.com>
2023-07-20 12:38:14 +0500

    showing qr codes

--- Modules/DeliveryService/Routes/web.php
+++ Modules/DeliveryService/Routes/web.php
@@ -16,6 +16,8 @@ Route::prefix('delivery')->group(function() {
 
     Route::group(['prefix' => 'bag/'], function () {
         Route::get('/', [Modules\DeliveryService\Http\Controllers\Bags\BagsController::class, "viewAllBags"])->name("view_all_bags");
+        Route::get('/bags', [Modules\DeliveryService\Http\Controllers\Bags\BagsController::class, "viewPartnerBag"])->name("view_partner_bags");
+
         Route::get('/add', [Modules\DeliveryService\Http\Controllers\Bags\BagsController::class, "addBag"])->name("add_new_bag");
         Route::post('/add/{partner_id}', [Modules\DeliveryService\Http\Controllers\Bags\BagsController::class, "storeBag"])->name("store_new_bag");
 
