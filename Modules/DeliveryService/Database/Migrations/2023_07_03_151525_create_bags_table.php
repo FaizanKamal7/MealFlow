ac8a7282e499d2f22ba97affe14154d19a537402
zeeshan <rajazeeshan410@gmail.com>
2023-08-22 09:40:37 +0500

    clearing faizan mess of migrations and also adding wallet creation upon business registration

--- Modules/DeliveryService/Database/Migrations/2023_07_03_151525_create_bags_table.php
+++ Modules/DeliveryService/Database/Migrations/2023_07_03_151525_create_bags_table.php
@@ -14,25 +14,27 @@ return new class extends Migration {
     {
         Schema::create('bags', function (Blueprint $table) {
             $table->uuid("id")->primary();
-            $table->uuid("business_id")->nullable();
+           
             $table->string("qr_code")->nullable();
             $table->string("bag_number")->nullable();
             $table->string("bag_size")->nullable();
             $table->string("bag_type")->nullable();
             // $table->string("status")->nullable();
-            $table->uuid("status_id")->nullable();
             
+            $table->uuid("status_id")->nullable();
+            $table->uuid("business_id")->nullable();
+
             $table->string("weight")->nullable();
             $table->string("dimensions")->nullable();
 
             $table->foreign("status_id")->references("id")->on("bag_statuses")->onDelete("set null");
-            $table->foreign("business_id")->references("id")->on("businesses")->onDelete("cascade");
+            $table->foreign("business_id")->references("id")->on("businesses")->onDelete("set null");
 
             //Partner ID
             $table->timestamps();
-            $table->uuid("business_id");
+         
 
-            $table->foreign("business_id")->references("id")->on("businesses")->onDelete("cascade");
+           
         });
     }
 
