1d8bf53be61aa52e17ef3f8a57db8a404306b556
FaizanKamal7 <57044214+FaizanKamal7@users.noreply.github.com>
2023-09-01 12:22:01 +0500

    UPDATE: Added softDelete in all migrations
                $table->softDeletes();

--- Modules/DeliveryService/Http/Exports/ExcelImportClass.php
+++ Modules/DeliveryService/Http/Exports/ExcelImportClass.php
@@ -1,24 +1,76 @@
 <?php
 
 namespace Modules\DeliveryService\Http\Exports;
+
+use App\Models\Area;
+use App\Models\City;
+use App\Models\User;
 use Maatwebsite\Excel\Concerns\OnEachRow;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Row;
+use Modules\BusinessService\Entities\CustomerAddress;
 use Modules\DeliveryService\Entities\Delivery;
 
-class ExcelImportClass implements  WithHeadingRow, onEachRow
+class ExcelImportClass implements WithHeadingRow, onEachRow
 {
+    protected $headerColumns = [];
+    // protected $expectedHeaders = ['Full Name'];
+    protected $expectedHeaders = [
+        'address',
+        'area_with_city_select_option',
+        'customerid_optional',
+        'email_optional',
+        'emirate_with_timeselect_option',
+        'full_name',
+        'google_link_address_optional',
+        'notes',
+        'notification_select_option',
+        'phone',
+        'pickup_point_optional',
+        'product_type_optional'
+    ]; // List of expected headers
+
 
     protected $selectedCustomers = [];
-
     public function onRow(Row $row)
     {
-
         $rowIndex = $row->getIndex();
         $sheetTitle = $row->getWorksheet()->getTitle();
 
-        if($sheetTitle === 'Worksheet'){
+
+        // Get the actual headers from the Excel file
+        $actualHeaders = $row->toArray();
+        // Compare actual headers with expected headers
+        if ($this->headersMatch($actualHeaders)) {
+            echo "<pre>" . $row['phone'] . "<pre>";
+            //TODO: Call repo fucntion here. 
+            $customer_db_id = User::where(['phone' => $row['phone']])->get();
+            $sheet_area_with_city = $row['area_with_city_select_option'];
+            $city_name = '';
+            $area_name = '';
+
+            $openingParenthesisPos = strpos($sheet_area_with_city, '(');
+
+            if ($openingParenthesisPos !== false) {
+                $city_name = substr($sheet_area_with_city, 0, $openingParenthesisPos);
+                $area_name = substr($sheet_area_with_city, $openingParenthesisPos + 1, -1);
+            }
+            $city = City::where(['name' => $city_name]);
+            $area = Area::where(['name' => $area_name]);
+            $customer_address = CustomerAddress::where(['customer_id' => $customer_db_id, 'city_id' => $city->id, 'state_id' => $area->id])->get();
+
+            echo "<pre>sheet " . $sheet_area_with_city . "<pre>";
+            echo "<pre>city " . $city->id . " " . $city->name . "<pre>";
+            echo "<pre>area " . $area->id . " " . $area->name . "<pre>";
+            echo "<pre>customer_address " . $customer_address . "<pre>";
+
+            dd($customer_db_id);
+        } else {
+            // Headers do not match, throw an exception or handle the error
+            throw new \Exception('Headers do not match expected format.');
+        }
+        if ($sheetTitle === 'Worksheet') {
             // Get the value of the customer cell in the current row
             $customer = $row->toArray()["customer"];
 
@@ -39,11 +91,10 @@ class ExcelImportClass implements  WithHeadingRow, onEachRow
                 // Value is not a duplicate, add it to the selectedOptions array
                 $this->selectedCustomers[] = $customer;
 
-//                dd($customer,$address,$deliverySlot,$item_type,$enableSMSNotifications,$enableEmailNotifications,$special_instructions,$notes,$codAmount);
 
                 //TODO::Push delivery data to database table (Delivery)
             }
-        }else if($sheetTitle == "New Customers"){
+        } else if ($sheetTitle == "New Customers") {
             // Import data from the "New Customers" sheet
             $customer = $row->toArray()["customer"];
 
@@ -64,7 +115,7 @@ class ExcelImportClass implements  WithHeadingRow, onEachRow
                 // Value is not a duplicate, add it to the selectedOptions array
                 $this->selectedCustomers[] = $customer;
 
-                dd($customer,$address,$deliverySlot,$item_type,$enableSMSNotifications,$enableEmailNotifications,$special_instructions,$notes,$codAmount);
+                dd($customer, $address, $deliverySlot, $item_type, $enableSMSNotifications, $enableEmailNotifications, $special_instructions, $notes, $codAmount);
 
                 // TODO: Push delivery data to the database table (Delivery)
             }
@@ -73,31 +124,50 @@ class ExcelImportClass implements  WithHeadingRow, onEachRow
         return $this->selectedCustomers;
     }
 
-//    public function model(array $row)
-//    {
-//        dd($row);
-//        die();
-//        return null;
-////        return new Delivery([
-////            'name' => $row['name'],
-////            'email' => $row['email'],
-////            'phone_number' => $row['phone_number'],
-////        ]);
-//    }
+    public function headingRow(): int
+    {
+        return 1;
+    }
 
-//    public function rules(): array
-//    {
-//        return [
-//            'name' => 'required|string|max:255',
-//            'email' => 'required|email|unique:delivery,email',
-//            'phone_number' => 'required|string|max:20',
-//        ];
-//    }
+    protected function headersMatch($actualHeaders)
+    {
 
-//    public function customValidationMessages()
-//    {
-//        return [
-//            'email.unique' => 'The email address :value is already taken.',
-//        ];
-//    }
+        $actualHeaders = array_keys($actualHeaders);
+        $actualHeadersLowercase = array_map('strtolower', $actualHeaders);
+        $expectedHeadersLowercase = array_map('strtolower', $this->expectedHeaders);
+
+        sort($actualHeadersLowercase);
+        sort($expectedHeadersLowercase);
+
+        return $actualHeadersLowercase === $expectedHeadersLowercase;
+    }
+
+
+    //    public function model(array $row)
+    //    {
+    //        dd($row);
+    //        die();
+    //        return null;
+    ////        return new Delivery([
+    ////            'name' => $row['name'],
+    ////            'email' => $row['email'],
+    ////            'phone_number' => $row['phone_number'],
+    ////        ]);
+    //    }
+
+    //    public function rules(): array
+    //    {
+    //        return [
+    //            'name' => 'required|string|max:255',
+    //            'email' => 'required|email|unique:delivery,email',
+    //            'phone_number' => 'required|string|max:20',
+    //        ];
+    //    }
+
+    //    public function customValidationMessages()
+    //    {
+    //        return [
+    //            'email.unique' => 'The email address :value is already taken.',
+    //        ];
+    //    }
 }
---
+++ Modules/DeliveryService/Repositories/DeliveryRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Modules\DeliveryService\Repositories;
+
+use Modules\DeliveryService\Interfaces\DeliveryInterface;
+
+class DeliveryRepository implements DeliveryInterface
+{
+}
