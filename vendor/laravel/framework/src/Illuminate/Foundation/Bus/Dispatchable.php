5d3c137fda44ac8b044079b3216f781f4e189926
FaizanKamal7 <57044214+FaizanKamal7@users.noreply.github.com>
2023-09-25 12:38:35 +0500

    Merged

--- vendor/laravel/framework/src/Illuminate/Foundation/Bus/Dispatchable.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Bus/Dispatchable.php
@@ -53,12 +53,12 @@ trait Dispatchable
         if ($boolean instanceof Closure) {
             $dispatchable = new static(...$arguments);
 
-            return !value($boolean, $dispatchable)
+            return ! value($boolean, $dispatchable)
                 ? new PendingDispatch($dispatchable)
                 : new Fluent;
         }
 
-        return !value($boolean)
+        return ! value($boolean)
             ? new PendingDispatch(new static(...$arguments))
             : new Fluent;
     }
