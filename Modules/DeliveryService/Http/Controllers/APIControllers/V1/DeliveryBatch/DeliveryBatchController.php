24c9025d798b655eafb8ad3c27ac27be23c89049
zeeshan <rajazeeshan410@gmail.com>
2023-09-26 13:36:49 +0500

    batch start and end api

--- Modules/DeliveryService/Http/Controllers/APIControllers/V1/DeliveryBatch/DeliveryBatchController.php
+++ Modules/DeliveryService/Http/Controllers/APIControllers/V1/DeliveryBatch/DeliveryBatchController.php
@@ -12,14 +12,12 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Validator;
-use Modules\BusinessService\Entities\CustomerAddress;
 use Modules\BusinessService\Interfaces\BranchInterface;
 use Modules\BusinessService\Interfaces\BusinessCustomerInterface;
 use Modules\BusinessService\Interfaces\BusinessInterface;
 use Modules\BusinessService\Interfaces\CustomerAddressInterface;
 use Modules\BusinessService\Interfaces\CustomerInterface;
 use Modules\BusinessService\Interfaces\CustomerSecondaryNumberInterface;
-use Modules\DeliveryService\Http\Exports\DeliveryTemplateClass;
 use Modules\DeliveryService\Interfaces\DeliveryBatchInterface;
 use Modules\DeliveryService\Interfaces\DeliveryInterface;
 use Modules\DeliveryService\Interfaces\DeliveryTypeInterface;
@@ -93,8 +91,9 @@ class DeliveryBatchController extends Controller
             // Validate the request data
             $validator = Validator::make($request->all(), [
                 'batch_id' => ['required'],
-                'start_time' => ['required'],
+                'start_time' => ['required', 'date'],
                 'vehicle_id' => ['required'],
+                'map_coordinates' => ['required'],
             ]);
 
             // Check if validation fails
@@ -105,8 +104,9 @@ class DeliveryBatchController extends Controller
             $batch_id = $request->get('batch_id');
             $start_time = $request->get('start_time');
             $vehicle_id = $request->get('vehicle_id');
+            $start_coordinates = $request->get('map_coordinates');
 
-            $data = ['batch_start_time' => $start_time, 'vehicle_id' => $vehicle_id];
+            $data = ['batch_start_time' => $start_time, 'vehicle_id' => $vehicle_id,,'status'=>'Pending', 'batch_arrival_map_coordinates' => $start_coordinates];
             $update = $this->deliveryBatchRepository->updateDeliveryBatch($batch_id, $data);
 
             if (!$update) {
@@ -114,6 +114,42 @@ class DeliveryBatchController extends Controller
             }
 
             return $this->success($update, "Batch Started successfully");
+
+        } catch (Exception $exception) {
+            return $this->error($exception, "error occured contact support", 500);
+
+        }
+    }
+
+    public function endDeliveryBatch(Request $request)
+    {
+
+        try {
+            // Validate the request data
+            $validator = Validator::make($request->all(), [
+                'batch_id' => ['required'],
+                'end_time' => ['required', 'date'],
+                'map_coordinates' => ['required'],
+            ]);
+
+            // Check if validation fails
+            if ($validator->fails()) {
+                return $this->error($validator->errors(), "validation failed", 422);
+            }
+
+            $batch_id = $request->get('batch_id');
+            $end_time = $request->get('end_time');
+            $vehicle_id = $request->get('vehicle_id');
+            $end_coordinates = $request->get('map_coordinates');
+
+            $data = ['batch_end_time' => $end_time, 'vehicle_id' => $vehicle_id,'status'=>'Completed', 'batch_end_map_coordinates' => $end_coordinates];
+            $update = $this->deliveryBatchRepository->updateDeliveryBatch($batch_id, $data);
+
+            if (!$update) {
+                return $this->error($update, "error occured contact support", 500);
+            }
+
+            return $this->success($update, "Batch Completed successfully");
 
         } catch (Exception $exception) {
             return $this->error($exception, "error occured contact support", 500);
