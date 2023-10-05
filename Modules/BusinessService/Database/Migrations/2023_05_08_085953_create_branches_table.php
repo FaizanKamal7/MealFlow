15f322e14793d6796265cb91baee94ae77614788
FaizanKamal7 <57044214+FaizanKamal7@users.noreply.github.com>
2023-10-05 17:20:07 +0500

    slight

--- Modules/BusinessService/Database/Migrations/2023_05_08_085953_create_branches_table.php
+++ Modules/BusinessService/Database/Migrations/2023_05_08_085953_create_branches_table.php
@@ -20,12 +20,12 @@ return new class extends Migration
             $table->string('phone')->nullable();
             $table->string('active_status');
             $table->boolean('is_main_branch');
-            $table->uuid('area_id');
-            $table->uuid('city_id');
-            $table->uuid('state_id');
             $table->string('map_selected_area')->nullable();
             $table->decimal('latitude', 10, 7)->nullable();  // 10 digits total, 7 after the decimal point
             $table->decimal('longitude', 10, 7)->nullable(); // 10 digits total, 7 after the decimal point
+            $table->uuid('area_id');
+            $table->uuid('city_id');
+            $table->uuid('state_id');
             $table->uuid('country_id');
             $table->uuid('business_id');
             $table->foreign('area_id')->references('id')->on('areas')->onDelete('cascade');
