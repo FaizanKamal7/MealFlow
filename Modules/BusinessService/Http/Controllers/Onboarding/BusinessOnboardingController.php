3335f994ddd565556cb056d3b4d9d2cea1e90cbc
FaizanKamal7 <57044214+FaizanKamal7@users.noreply.github.com>
2023-10-19 15:36:01 +0500

    Merge

--- Modules/BusinessService/Http/Controllers/Onboarding/BusinessOnboardingController.php
+++ Modules/BusinessService/Http/Controllers/Onboarding/BusinessOnboardingController.php
@@ -16,6 +16,7 @@ use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Modules\BusinessService\Interfaces\BranchCoverageInterface;
 use Modules\BusinessService\Interfaces\BranchInterface;
