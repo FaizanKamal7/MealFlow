251ec066600f33a0be3d049b5a9d758b2b64ea82
FaizanKamal7 <57044214+FaizanKamal7@users.noreply.github.com>
2023-11-07 14:45:53 +0500

    Removed Breeze and Sanctum

--- routes/web.php
+++ routes/web.php
@@ -28,9 +28,9 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboards.admin_dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::group(['middleware' => 'auth'], function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -114,7 +114,6 @@ Route::middleware('auth')->group(function () {
     });
 });
 
-require __DIR__ . '/auth.php';
 
 
 
