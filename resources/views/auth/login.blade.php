2eda7abe57c053216f480e835e2a8a394ec83667
Subyyal99 <siddiquisubyyal@gmail.com>
2023-11-16 16:43:28 +0500

    Login Screen
    light and dark mode

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,80 +4,54 @@
 
 @section('main_content')
 
-@if ($errors->any())
-<p class="alert {{ Session::get('alert-class') }}">
-    @foreach ($errors->all() as $error)
-    {{ $error }}
-    @endforeach
-</p>
-@endif
+    @if ($errors->any())
+        <p class="alert {{ Session::get('alert-class') }}">
+            @foreach ($errors->all() as $error)
+                {{ $error }}
+            @endforeach
+        </p>
+    @endif
 
-<!--begin::Root-->
-<div class="d-flex flex-column flex-root bg-login">
-    <!--begin::Authentication - Sign-in -->
-    <div class="d-flex flex-column flex-lg-row flex-column-fluid margin-div">
-        <!--begin::Aside-->
-        <div class="d-flex flex-column flex-lg-row-auto w-xl-500px positon-xl-relative rounded-lg side-div">
-            <!--begin::Wrapper-->
-            <div class="d-flex flex-column position-xl-fixed w-xl-500px ">
-                <!--begin::Content-->
-                <div class="d-flex flex-row-fluid flex-column p-10">
-
-                    <div class="welcome mt-xxl-16">
-                        <!--begin::Logo-->
-                        <a href="#" class="pb-9">
-                            <img alt="Logo" src="{{ asset('static/media\logos\logo_.png') }}" class="h-90px" />
-                        </a>
-                        <!--end::Title-->
-                        <!--begin::Description-->
-                        <p class="fw-bold fs-2" style="color: #00426e;">One stop solution
-                            <br />for all your logistics needs
-                        </p>
-                    </div>
-                    <!--begin::Title-->
-                    <!--end::Description-->
-                </div>
-                <!--end::Content-->
-                <!--begin::Illustration-->
-                {{-- <div
-                    class="d-flex flex-row-auto bgi-no-repeat bgi-position-x-center bgi-size-contain bgi-position-y-bottom min-h-100px min-h-lg-290px"
-                    style="background-image: url({{ asset('static/media/Isolation_Mode.png') }})">
-                </div> --}}
+    <!--begin::Root-->
+    <div class="bg-login">
+        <!--begin::Authentication - Sign-in -->
+        <div class="d-flex margin-div">
+            <!--begin::Aside-->
+            <div class="d-flex justify-content-center align-items-center positon-xl-relative rounded-lg side-div">
                 <div class="big-img">
-                    <img alt="bigImage" class="h-xxl-300px img-big"
-                        src="{{ asset('static/media/Isolation_Mode.png') }}" />
-
+                    <img alt="bigImage" class="img-big" src="{{ asset('static/media/login-side.png') }}" />
                 </div>
-                <!--end::Illustration-->
-            </div>
-            <!--end::Wrapper-->
-        </div>
-        <!--end::Aside-->
-        <!--begin::Body-->
-        <div class="d-flex flex-column flex-lg-row-fluid login-div">
-            <!--begin::Content-->
-            <div class="d-flex flex-center flex-column flex-column-fluid">
                 <!--begin::Wrapper-->
-                <div class="w-lg-500px p-10 mx-auto">
+                <!--end::Wrapper-->
+            </div>
+            <!--end::Aside-->
+            <!--begin::Body-->
+            <div class="login-div">
+                <!--begin::Content-->
+                <div class="big-img" style="width: max-content;  margin: 0 auto;">
+                    <img alt="bigImage" style="height: 170px;" class=""
+                        src="{{ asset('static/media/lightLogo.png') }}" />
+                </div>
+                <!--begin::Wrapper-->
+                <div class="mx-10">
                     <!--begin::Form-->
                     <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" method="POST"
                         action="{{ route('login') }}">
                         @csrf
                         <!--begin::Heading-->
-                        <div class="mb-10">
+                        <div class="mb-5">
                             <!--begin::Title-->
-                            <h1 class="text-dark mb-3">Login</h1>
+                            <h1 class="text-dark login-text">Login</h1>
                             <!--end::Title-->
                         </div>
-
                         <!--begin::Heading-->
                         @if (Session::has('error'))
-                        <p class="alert {{ Session::get('alert-class') }}">{{ Session::get('error') }}</p>
+                            <p class="alert {{ Session::get('alert-class') }}">{{ Session::get('error') }}</p>
                         @endif
                         <!--begin::Input group-->
-                        <div class="fv-row mb-10">
+                        <div class="fv-row mb-2">
                             <!--begin::Label-->
-                            <label for="login" class="form-label required fs-6 fw-bolder text-dark">Email/Phone</label>
+                            <label for="login" class="login-text form-label required fs-6 fw-bolder text-dark">Email/Phone</label>
                             <!--end::Label-->
                             <!--begin::Input-->
                             <input class="form-control form-control-lg form-control-solid" type="text" id="login"
@@ -86,11 +60,11 @@
                         </div>
                         <!--end::Input group-->
                         <!--begin::Input group-->
-                        <div class="fv-row mb-10">
+                        <div class="fv-row mb-2">
                             <!--begin::Wrapper-->
-                            <div class="mb-10">
+                            <div class="mb-2">
                                 <!--begin::Label-->
-                                <label class="form-label required fw-bolder text-dark fs-6 mb-0">Password</label>
+                                <label class="login-text form-label required fw-bolder text-dark fs-6 mb-0">Password</label>
                                 <!--end::Label-->
                                 <!--begin::Link-->
                                 <!--begin::Input-->
@@ -104,16 +78,16 @@
                                     <input id="remember_me" type="checkbox"
                                         class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
                                         name="remember">
-                                    <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me')
-                                        }}</span>
+                                    <span
+                                        class="login-text ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
                                 </label>
                             </div>
                             <div class="flex items-center justify-end mt-4">
                                 @if (Route::has('password.request'))
-                                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                                    href="{{ route('password.request') }}">
-                                    {{ __('Forgot your password?') }}
-                                </a>
+                                    <a class="login-text underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                                        href="{{ route('password.request') }}">
+                                        {{ __('Forgot your password?') }}
+                                    </a>
                                 @endif
 
 
@@ -125,7 +99,8 @@
                         <!--begin::Actions-->
                         <div class="text-center">
                             <!--begin::Submit button-->
-                            <button type="submit" id="kt_sign_in_submit" class="submit_btn btn btn-lg  w-100 mb-5">
+                            <button type="submit" id="kt_sign_in_submit"
+                                class="submit_btn btn btn-primary btn-lg  w-100 mb-5">
                                 {{ __('Log in') }}
                                 <span class="indicator-progress">Please wait...
                                     <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
@@ -139,37 +114,21 @@
                             Don’t have an account? <a href="{{ route('business_onboarding') }}">
                                 <span class="link-primary"> Register</span> </p>
                         </a>
-
-
                     </div>
                 </div>
-                <!--end::Wrapper-->
             </div>
-            <!--end::Content-->
-            <!--begin::Footer-->
-            {{-- <div class="d-flex flex-center flex-wrap fs-6 p-5 pb-0">
-                <!--begin::Links-->
-                <div class="d-flex flex-center fw-bold fs-6">
-                    <a href="#" class="text-muted text-hover-primary px-2" target="_blank">About</a>
-                    <a href="#" class="text-muted text-hover-primary px-2" target="_blank">Support</a>
-                    <a href="#" class="text-muted text-hover-primary px-2" target="_blank">Purchase</a>
-                </div>
-                <!--end::Links-->
-            </div> --}}
-            <!--end::Footer-->
+            <!--end::Body-->
         </div>
-        <!--end::Body-->
+        <!--end::Authentication - Sign-in-->
     </div>
-    <!--end::Authentication - Sign-in-->
-</div>
-<!--end::Root-->
+    <!--end::Root-->
 
 @endsection
 
 @section('extra_scripts')
-<script src="{{ asset('static/js/custom/authentication/sign-in/general.js') }}"></script>
-<script>
-    $(document).ready(function() {
+    <script src="{{ asset('static/js/custom/authentication/sign-in/general.js') }}"></script>
+    <script>
+        $(document).ready(function() {
             $('#kt_docs_repeater_basic').repeater({
                 initEmpty: false,
 
@@ -186,5 +145,5 @@
                 }
             });
         });
-</script>
+    </script>
 @endsection
